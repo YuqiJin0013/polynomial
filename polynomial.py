782ae39b1a82a1fd23ed0bfb38d6b79ffe8f457a
Yuqi Jin <yuqijin8@bu.edu>
2024-01-27 11:37:18 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,6 +5,9 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x):
+        return x
+    
 class Int:
     def __init__(self, i):
         self.i = i
@@ -12,6 +15,10 @@ class Int:
     def __repr__(self):
         return str(self.i)
 
+    def evaluate(self, x):
+        return self.i
+    
+    
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -20,6 +27,9 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
+    
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -34,6 +44,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -42,6 +55,13 @@ class Div:
     def __repr__(self):
         return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
 
+    def evaluate(self, x):
+        try:
+            return self.p1.evaluate(x) / self.p2.evaluate(x)
+        except ZeroDivisionError:
+            print("Error: Division by zero")
+            return None
+        
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -50,5 +70,8 @@ class Sub:
     def __repr__(self):
         return "( " + repr(self.p1) + " ) - ( " + repr(self.p2) + " )"
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
+    
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
+print(poly.evaluate(-1))
